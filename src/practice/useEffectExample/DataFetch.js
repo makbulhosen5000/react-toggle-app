2b32b892715bd5677ca6c382f83d8c67e374875c
mhakash5000 <mhakash5000@gmail.com>
2022-09-09 21:30:55 +0600

    updated some files

--- src/practice/useEffectExample/DataFetch.js
+++ src/practice/useEffectExample/DataFetch.js
@@ -1,10 +1,22 @@
 import React, { useEffect, useState } from 'react'
 
 function DataFetch() {
-    
+    const [todos,setTodos] = useState(null);
+    useEffect(()=>{
+      fetch("https://jsonplaceholder.typicode.com/todos")
+      .then((res)=>{
+        return res.json()
+      })
+      .then((data)=>{
+        setTodos(data);
+      })
+    })
   return (
     <div>
         <h1>Todos</h1>
+        {todos && todos.map((todo,index)=>(
+          <p key={index}>{todo.title}</p>
+        ))}
         
     </div>
   )
