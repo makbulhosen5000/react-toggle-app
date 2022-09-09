9462e30d4cce43274a493e03b016e8f4ea6eb2fb
mhakash5000 <mhakash5000@gmail.com>
2022-09-09 22:14:41 +0600

    added catching error

--- src/practice/useEffectExample/DataFetch.js
+++ src/practice/useEffectExample/DataFetch.js
@@ -1,22 +1,42 @@
 import React, { useEffect, useState } from 'react'
+ 
+const dataLoading = "Data is Loading";
 
 function DataFetch() {
-    const [todos,setTodos] = useState(null);
-    useEffect(()=>{
-      fetch("https://jsonplaceholder.typicode.com/todos")
-      .then((res)=>{
-        return res.json()
-      })
-      .then((data)=>{
-        setTodos(data);
-      })
+  const [todos,setTodos] = useState(null);
+  const [isLoading,setIsLoading] = useState(true);
+  const [isError,setIsError] = useState(null);
+   useEffect(()=>{
+    fetch("https://jsonplaceholder.typicode.com/todos")
+    .then((res)=>{
+      if(!res.ok){
+        throw Error("Fetching is not Successful");
+      }else{
+        return res.json();
+      }
+
     })
+    .then((data)=>{
+      setTodos(data);
+      setIsLoading(false);
+    })
+    .catch((error)=>{
+        setIsError(error.message)
+        setIsLoading(false);
+    })
+   },[])
+   
+   const todoElement =
+   todos && todos.map((todo)=>(
+    <p key={todo.id}>{todo.title}</p>
+  ))
   return (
     <div>
         <h1>Todos</h1>
-        {todos && todos.map((todo,index)=>(
-          <p key={index}>{todo.title}</p>
-        ))}
+        {isLoading&& dataLoading}
+        {todoElement}
+        {isError&& <p>{isError}</p>}
+        
         
     </div>
   )
